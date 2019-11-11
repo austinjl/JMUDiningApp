68c7eeb28a846ce980b8319bc13dbd1b83c78a5d
austinjl <alam@consultwithcase.com>
2019-11-10 22:36:33 -0500

    1: added dining locations and addresses

---
+++ script/LogIn.js
@@ -0,0 +1,5 @@
+function toDiningLocations() {
+  //window.location.href = "file:///Users/austinlam/Documents/CIS454/CaseProject4/JMUDiningApp/views/DiningLocations.html";
+  //alert("hello world");
+  window.location.href = "DiningLocations.html";
+}
