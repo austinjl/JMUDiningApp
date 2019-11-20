ba51caf8e345ed6e0898437b9f208b6f1d79439c
austinjl <alam@consultwithcase.com>
2019-11-20 17:13:29 -0500

    8. Connect Login to Main/Home Page

--- script/LogIn.js
+++ script/LogIn.js
@@ -1,7 +1,7 @@
-function toDiningLocations() {
+function toHomePage() {
   //window.location.href = "file:///Users/austinlam/Documents/CIS454/CaseProject4/JMUDiningApp/views/DiningLocations.html";
   //alert("hello world");
-  window.location.href = "DiningLocations.html";
+  window.location.href = "HomePage.html";
 }
 
 function toCreateAccount() {
